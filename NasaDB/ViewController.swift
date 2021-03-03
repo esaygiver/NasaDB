af430474a60553f4675ee973334b102b4d3a0c54
esaygiver <saygivere@gmail.com>
2021-03-03 16:56:38 +0300

    Pods Added

---
+++ NasaDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NasaDB
+//
+//  Created by admin on 3.03.2021.
+//  Copyright © 2021 esaygiver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '13.0'
+
+target 'NasaDB' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NasaDB
+
+pod 'Moya', '~> 14.0'
+pod 'Kingfisher', '~> 6.0'
+
+end
